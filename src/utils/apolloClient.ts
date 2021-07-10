fbc6845ffd9c772137dd2a29c6f59491b9a9b66c
obrin <jordanyong23@gmail.com>
2021-07-10 23:59:55 +0200

    Abstract URI

--- src/utils/apolloClient.ts
+++ src/utils/apolloClient.ts
@@ -6,8 +6,10 @@ import {
 
 export const ApolloProvider = _ApolloProvider
 
+const URI = process.env.API_URI || 'http://api.home.local/graphql'
+
 export const client = new ApolloClient({
-  uri: 'http://api.home.local/graphql',
+  uri: URI,
   cache: new InMemoryCache()
 });
 

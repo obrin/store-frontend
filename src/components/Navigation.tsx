f5fdfb62a5633190edcabce5f85e97549734ca60
obrin <jordanyong23@gmail.com>
2021-07-11 14:11:38 +0200

    Change navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,30 +1,42 @@
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
-import Container from '@material-ui/core/Container'
-import { Link } from 'react-router-dom'
+import { Link as RouterLink } from 'react-router-dom'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import Link from '@material-ui/core/Link'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-const Navigation = () => (
-  <Navbar bg="dark" expand="lg" variant="dark">
-    <Container maxWidth="md">
-      <Link to="/">
-        <Navbar.Brand>Shop.com</Navbar.Brand>
-      </Link>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav>
-          <NavDropdown title="Account" id="collapsible-nav-dropdown">
-            <Nav.Item>
-              <Link to="/profile">Profile</Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Link to="/wishlist">Wishlist</Link>
-            </Nav.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-)
+const Navigation = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+      <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          News
+        </Typography>
+
+        <Link component={RouterLink} to="/">
+          Shop.com
+        </Link>
+        <Link component={RouterLink} to="/profile">
+          Profile
+        </Link>
+      </Toolbar>
+    </AppBar>
+  )
+}
 
 export default Navigation

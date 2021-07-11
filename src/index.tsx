ae03f9c12614010915b73d7813c46a96393046a2
obrin <jordanyong23@gmail.com>
2021-07-11 15:06:54 +0200

    Remove bootstrap css

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App'
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
 
 
 ReactDOM.render(

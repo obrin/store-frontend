cb43372c0263d5e9c2779a54d61081cf1856c08e
obrin <jordanyong23@gmail.com>
2021-07-11 14:56:16 +0200

    Replace bootstrap for material-ui

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,17 @@
-import Spinner from 'react-bootstrap/Spinner'
 import gql from 'graphql-tag'
 import { useQuery } from '@apollo/react-hooks'
 import Container from '@material-ui/core/Container'
-import { experimentalStyled as styled } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import { experimentalStyled as styled } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import Box from '@material-ui/core/Box'
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(2),
   textAlign: 'center',
   color: theme.palette.text.secondary,
-}));
-
+}))
 
 export const TOP_PRODUCT_QUERY = gql`
   query topProducts($first: Int!) {
@@ -33,20 +32,27 @@ const HomePage = () => {
   if (loading) {
     return (
       <Container maxWidth="md">
-        <Spinner animation="border" />
+        Loading...
       </Container>
     )
   }
 
   return (
     <Container maxWidth="md">
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-        {data.topProducts.map(({name}, index) => (
+      <Box sx={{padding: 5}}>
+
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        {data.topProducts.map(({ name }, index) => (
           <Grid item xs={2} sm={4} md={4} key={index}>
             <Item>{name}</Item>
           </Grid>
         ))}
       </Grid>
+      </Box>
     </Container>
   )
 }
